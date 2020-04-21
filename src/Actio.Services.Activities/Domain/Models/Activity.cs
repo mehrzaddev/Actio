fd325451a159f33a6e72745f327c998b2e119e82
mhrxadev <60345056+mhrxadev@users.noreply.github.com>
2020-04-21 15:20:05 +0430

    Create Domain folder in Actio.Services.Activities &Create Activity, Category Models & Repository

---
+++ src/Actio.Services.Activities/Domain/Models/Activity.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Actio.Services.Activities.Domain.Models
+{
+    public class Activity
+    {
+        public Guid Id { get; protected set; }
+        public string Category { get; protected set; }
+        public Guid UserId { get; protected set; }
+        public string Name { get; protected set; }
+        public string Description { get; protected set; }
+        public DateTime CreateAt { get; protected set; }
+
+        public Activity()
+        {
+
+        }
+
+        public Activity(Guid id, Category category, Guid userId,
+            string name, string description, DateTime createAt)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new Exception();
+            }
+
+            Id = id;
+            Category = category.Name;
+            UserId = userId;
+            Name = name;
+            Description = description;
+            CreateAt = createAt;
+        }
+
+
+
+    }
+}
