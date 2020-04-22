e0cbd418464b0e334ac415736ac0dab9f8d8018f
mhrxadev <60345056+mhrxadev@users.noreply.github.com>
2020-04-22 13:09:04 +0430

    Edit Handler in Actio.Services.Activities

--- src/Actio.Services.Activities/Handlers/CreateActivityHandler.cs
+++ src/Actio.Services.Activities/Handlers/CreateActivityHandler.cs
@@ -3,26 +3,53 @@ using Actio.Common.Commands;
 using RawRabbit;
 using System;
 using Actio.Common.Events;
+using Actio.Services.Activities.Services;
+using Microsoft.Extensions.Logging;
+using Actio.Common.Exeptions;
 
 namespace Actio.Services.Activities.Handlers
 {
     public class CreateActivityHandler : ICommandHandler<CreateActivity>
     {
         private readonly IBusClient _busClient;
-        public CreateActivityHandler(IBusClient busClient)
+        private readonly IActivityService _activityService;
+        private readonly ILogger _logger;
+
+        public CreateActivityHandler(IBusClient busClient, IActivityService activityService,
+            ILogger logger)
         {
+            _logger = logger;
             _busClient = busClient;
+            _activityService = activityService;
         }
         public async Task HandleAsync(CreateActivity command)
         {
-           Console.WriteLine($"Creating Activity : '{command.Id}' for user: '{command.UserId}'");
-           await _busClient.PublishAsync(new ActivityCreated(command.Id,
-           command.UserId,
-           command.Category,
-           command.Name,
-           command.Description,
-           command.CreatedAt
-           ));
+            _logger.LogInformation($"Creating Activity : '{command.Id}' for user: '{command.UserId}'");
+
+            try
+            {
+                await _activityService.AddAsync(command.Id, command.UserId,
+                    command.Category, command.Name, command.Description, command.CreatedAt);
+                await _busClient.PublishAsync(new ActivityCreated(command.Id,
+                    command.UserId,
+                    command.Category,
+                    command.Name,
+                    command.Description,
+                    command.CreatedAt
+                ));
+                _logger.LogInformation($"Activity {command.Id} was create for user {command.UserId}");
+                return;
+            }
+            catch (ActioExeption ex)
+            {
+                _logger.LogError(ex, ex.Message);
+                await _busClient.PublishAsync(new CreateActivityRejected(command.Id, ex.Message, ex.Code));
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+                await _busClient.PublishAsync(new CreateActivityRejected(command.Id, ex.Message, "error"));
+            }
         }
     }
 }
